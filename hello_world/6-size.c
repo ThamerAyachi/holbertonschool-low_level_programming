54db8759ed7d6fda986f8deabc010294cff7c989
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-29 13:20:18 +0100

    add 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-    char myChar;
-    int myInt;
-    long int myLongInt;
-    long long int myLongLongInt;
-    float myFloat;
+char myChar;
+int myInt;
+long int myLongInt;
+long long int myLongLongInt;
+float myFloat;
 
-    printf("Size of a char: %zu byte(s)\n", sizeof(myChar));
-    printf("Size of an int: %zu byte(s)\n", sizeof(myInt));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(myLongInt));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(myLongLongInt));
-    printf("Size of a float: %zu byte(s)\n", sizeof(myFloat));
+printf("Size of a char: %zu byte(s)\n", sizeof(myChar));
+printf("Size of an int: %zu byte(s)\n", sizeof(myInt));
+printf("Size of a long int: %zu byte(s)\n", sizeof(myLongInt));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(myLongLongInt));
+printf("Size of a float: %zu byte(s)\n", sizeof(myFloat));
 
-    return (0);
+return (0);
 }
