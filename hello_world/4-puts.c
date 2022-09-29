f3299cd87039af0e1ad66aeedc05d9702d510228
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-29 12:50:24 +0100

    add 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Just print string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
