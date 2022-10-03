7de915dca5c9acfbd16cc725b8c82a9d55d2c772
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-03 13:01:02 +0100

    add 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints all the numbers of base 16 in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
