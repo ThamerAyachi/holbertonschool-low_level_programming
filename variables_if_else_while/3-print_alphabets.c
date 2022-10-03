f9a447089091634c74fb681621d0d0e5a565041f
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-03 12:29:25 +0100

    add 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 65; i <= 90; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
 	}
-	for (i = 97; i <= 122; i++)
+	for (i = 65; i <= 90; i++)
 	{
 		putchar(i);
 	}
