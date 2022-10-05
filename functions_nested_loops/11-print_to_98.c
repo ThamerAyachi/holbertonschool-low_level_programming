7cf41b816c7c596c135c7af47408d448d1ad2095
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-05 12:50:00 +0100

    add 11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n : integer
+ *
+ * Description: 'prints all natural numbers from n to 98.'
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+	char c, k, w;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i > 99)
+			{
+				w = (i / 100) + '0';
+				k = ((i / 10) - (i / 100) * 10) + '0';
+				c = (i % 10) + '0';
+				putchar(w);
+				putchar(k);
+				putchar(c);
+			}
+			else if (i > 9)
+			{
+				c = (i % 10) + '0';
+				k = (i / 10) + '0';
+				putchar(k);
+				putchar(c);
+			}
+			else
+				putchar(i + '0');
+
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i > 99)
+			{
+				w = (i / 100) + '0';
+				k = ((i / 10) - (i / 100) * 10) + '0';
+				c = (i % 10) + '0';
+
+				putchar(w);
+				putchar(k);
+				putchar(c);
+			}
+			else if (i > 9)
+			{
+				c = (i % 10) + '0';
+				k = (i / 10) + '0';
+				putchar(k);
+				putchar(c);
+			}
+			else
+				putchar(i + '0');
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -13,3 +13,4 @@
 | 8-24_hours.c           | Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59. |
 | 9-times_table.c        | Write a function that prints the 9 times table, starting with 0.                                  |
 | 10-add.c               | Write a function that adds two integers and returns the result.                                   |
+| 11-print_to_98.c       | Write a function that prints all natural numbers from `n` to `98`, followed by a new line.        |
