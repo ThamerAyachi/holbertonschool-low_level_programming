5a723f51242f92450b9ba00fe7bc319d4b26e37e
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-05 10:28:10 +0100

    add 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a : integer
+ * @b : integer
+ *
+ * Description: 'adds two integers and returns the result.'
+ *
+ * Return: integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
