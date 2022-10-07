bc97ae25cbb3d4dee0656706c3bd8c11e7cfd85c
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-07 22:38:42 +0100

    add 104-fibonacci.c

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print numbers'
+ *
+ * Return: void
+ */
+int main(void)
+{
+	double i, z, x, j;
+
+	j = 1;
+	z = 1;
+
+	for (i = 1; i <= 98; i++)
+	{
+		x = z;
+		z = j;
+		printf("%.0f", j);
+		j = x + z;
+		if (i != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
