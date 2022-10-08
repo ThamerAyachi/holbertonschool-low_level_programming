e664303848dfb6f8e0b914bd6ae758e5188a0e2c
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-08 23:12:28 +0100

    add

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 1231952;
+	int a = 1231952;
+	int i, j, t;
+
+	for (i = 2; i <= a; i++)
+	{
+		t = 0;
+		for (j = 0; j < i; j++)
+		{
+			if (j % i == 0 && j != i && j != 0 && j != 1)
+			{
+				t++;
+			}
+		}
+
+		if (t == 0 && n % i == 0)
+		{
+			printf("%d", i);
+			putchar(',');
+			putchar(' ');
+			n = n / i;
+		}
+	}
+
+	return (0);
+}
