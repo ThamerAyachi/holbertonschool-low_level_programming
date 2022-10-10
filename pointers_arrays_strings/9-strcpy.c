6b5c8a932a546fe35083519fcecf630f74e5def3
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-10 18:04:36 +0100

    add 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - Entry point
+ *
+ * @dest : pointer of string
+ * @src : pointer of string
+ *
+ * Description: 'copy string.'
+ *
+ * Return: pointer of string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len += 1;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
