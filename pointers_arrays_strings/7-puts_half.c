f40da854a939b0522d731d65d02899c312b942a1
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-10 17:24:20 +0100

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -19,9 +19,19 @@ void puts_half(char *str)
 		len += 1;
 	}
 
-	for (i = (len / 2); i < len; i++)
+	if (len % 2 == 0)
 	{
-		_putchar(str[i]);
+		for (i = (len / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = ((len - 1) / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
