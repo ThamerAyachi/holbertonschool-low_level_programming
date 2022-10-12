3d88e6ee6f815248bd70cad82775710ea8b36503
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-12 11:00:50 +0100

    add 3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - Entry point
+ *
+ * @dest : pointer of string
+ * @src : pointer of string
+ *
+ * Description: 'compares two strings.'
+ *
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = sizeof(s1);
+	j = sizeof(s2);
+
+	return (s1 - s2);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -21,3 +21,4 @@
 | 0-strcat.c  | Write a function that concatenates two strings. |
 | 1-strncat.c | Write a function that concatenates two strings. |
 | 2-strncpy.c | Write a function that copies a string.          |
+| 3-strcmp.c  | Write a function that compares two strings.     |
