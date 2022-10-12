a01f27e7f4c8d1c765fd097c46257008a47bbc14
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-12 11:36:19 +0100

    add 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j = 0;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
 	{
-		j += (s1[i] - s2[i]);
+		i += 1;
 	}
 
-	return (j);
+	return (*(s1 + i) - *(s2 + i));
 }
