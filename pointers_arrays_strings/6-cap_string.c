21e1af2270eab6a83814c8621365ed2b89994385
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-12 17:31:52 +0100

    add 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,31 +14,24 @@ char *cap_string(char *s)
 	int i, j;
 
 	if (s[0] >= 'a' && s[0] <= 'z')
-	{
-		j = s[0];
-		j -= 32;
-		s[0] = j;
-	}
+		s = string_toupper(s[0]);
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == '	')
+		if (s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '{')
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				j = s[i + 1];
-				j -= 32;
-				s[i + 1] = j;
-			}
+				s = string_toupper(s[0]);
 		}
-		else if (s[i] == '\\' && (s[i + 1] == 't' || s[i + 1] == 'n'))
+		else if (s[i] == '\n' || s[i] == '\t' || s[i] == '!' || s[i] == '?')
 		{
-			if (s[i + 2] >= 'a' && s[i + 2] <= 'z')
-			{
-				j = s[i + 2];
-				j -= 32;
-				s[i + 2] = j;
-			}
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s = string_toupper(s[0]);
+		}
+		else if (s[i] == ')' || s[i] == '}' || s[i] == '(' || s[i] == '"')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s = string_toupper(s[0]);
 		}
 	}
 
