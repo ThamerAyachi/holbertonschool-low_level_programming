ca38551bcfab71a8b891cecb3033de26f5a62a9e
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-12 17:21:57 +0100

    add 6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Entry point
+ *
+ * @s : string
+ *
+ * Description: 'to uppercase'
+ *
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				j = s[i + 1];
+				j -= 32;
+				s[i + 1] = j;
+			}
+		}
+		else if (s[i] == '\\' && (s[i + 1] == 't' || s[i + 1] == 'n'))
+		{
+			if (s[i + 2] >= 'a' && s[i + 2] <= 'z')
+			{
+				j = s[i + 2];
+				j -= 32;
+				s[i + 2] = j;
+			}
+		}
+	}
+
+	return (s);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -24,3 +24,4 @@
 | 3-strcmp.c         | Write a function that compares two strings.                                   |
 | 4-rev_array.c      | Write a function that reverses the content of an array of integers.           |
 | 5-string_toupper.c | Write a function that changes all lowercase letters of a string to uppercase. |
+| 6-cap_string.c     | Write a function that capitalizes all words of a string.                      |
