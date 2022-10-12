bf3ab63cffebc51937fbb22a19504303bb03ae85
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-12 09:40:40 +0100

    add 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ *
+ * @dest : pointer of string
+ * @src : pointer of string
+ * @n : integer
+ *
+ * Description: 'copy strings.'
+ *
+ * Return: pointer of string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -20,3 +20,4 @@
 | ----------- | ----------------------------------------------- |
 | 0-strcat.c  | Write a function that concatenates two strings. |
 | 1-strncat.c | Write a function that concatenates two strings. |
+| 2-strncpy.c | Write a function that copies a string.          |
