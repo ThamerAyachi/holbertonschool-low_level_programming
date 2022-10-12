1859a11edfe10ac1417459baf00363c9264e713c
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-12 17:36:32 +0100

    add 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * toUpper - function
+ *
+ * @s : character
+ *
+ * Description: 'to uppercase'
+ *
+ * Return: character
+ */
+char toUpper(char s)
+{
+	int j = s;
+
+	if (s >= 'a' && s <= 'z')
+	{
+		j -= 32;
+		s = j;
+	}
+
+	return (j);
+}
+
+/**
  * cap_string - Entry point
  *
  * @s : string
@@ -13,25 +35,21 @@ char *cap_string(char *s)
 {
 	int i, j;
 
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s = string_toupper(s[0]);
+	s[0] = toUpper(s[0]);
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '{')
 		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s = string_toupper(s[0]);
+			s[i + 1] = toUpper(s[i + 1]);
 		}
 		else if (s[i] == '\n' || s[i] == '\t' || s[i] == '!' || s[i] == '?')
 		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s = string_toupper(s[0]);
+			s[i + 1] = toUpper(s[i + 1]);
 		}
 		else if (s[i] == ')' || s[i] == '}' || s[i] == '(' || s[i] == '"')
 		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s = string_toupper(s[0]);
+			s[i + 1] = toUpper(s[i + 1]);
 		}
 	}
 
