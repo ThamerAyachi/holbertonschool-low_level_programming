13c3cd5d1d9943a4b9020ca6c9ee7e8d866b5e1f
ThamerAyachi <thamerjak3@gmail.com>
2022-10-13 16:04:02 +0100

    add 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ *
+ * @haystack : pointer of string
+ * @needle : pointer of string
+ *
+ * Description: 'locates a substring.'
+ *
+ * Return: unsignes integer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; *(haystack + i); i++)
+	{
+		for (j = 0; *(needle + j); j++)
+		{
+			if (*(needle + i) != *(haystack + j))
+				break;
+		}
+		if (!(needle + j))
+			break;
+	}
+
+	return (i);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -36,3 +36,4 @@
 | 2-strchr.c  | Write a function that locates a character in a string.             |
 | 3-strspn.c  | Write a function that gets the length of a prefix substring.       |
 | 4-strpbrk.c | Write a function that searches a string for any of a set of bytes. |
+| 5-strstr.c  | Write a function that locates a substring.                         |
