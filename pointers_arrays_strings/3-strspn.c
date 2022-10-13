dc5cb4ae6d63992623c2575712be9323415e7ede
ThamerAyachi <thamerjak3@gmail.com>
2022-10-13 13:31:29 +0100

    add gets the length of a prefix substring.

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,11 +14,11 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, x = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (s[j] == accept[i])
 				x++;
 		}
 	}
