c96161dfe9b3b6b2d5179ea5e940026b22a7cc80
ThamerAyachi <Thamerjak3@gmail.com>
2022-10-17 19:05:36 +0100

    add 101-wildcmp.c

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - Entry point
+ *
+ * @s1 : string
+ * @s2 : string
+ *
+ * Description: 'compares two strings.'
+ *
+ * Return: integer
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
