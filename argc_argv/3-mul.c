0eda85f455342da471cf127a774d728f534bb8a0
ThamerAyachi <thamerjak3@gmail.com>
2022-10-19 11:51:13 +0100

    add 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: 'prints the number of arguments passed into it.'
+ *
+ * Return: Always 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, r = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		r = r * argv[i];
+	}
+
+	printf("%d\n", r);
+
+	return (1);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -5,3 +5,4 @@
 | 0-whatsmyname.c | Write a program that prints its name, followed by a new line.       |
 | 1-args.c        | Write a program that prints the number of arguments passed into it. |
 | 2-args.c        | Write a program that prints all arguments it receives.              |
+| 3-mul.c         | Write a program that multiplies two numbers.                        |
