1b32b10b543fc15f1f5701949cf932b3bf910888
ThamerAyachi <thamerjak3@gmail.com>
2022-10-19 11:40:33 +0100

    add 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: 'prints the number of arguments passed into it.'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	int i = 0;
@@ -9,7 +19,7 @@ int main(int argc, char *argv[])
 		i++;
 	}
 
-	printf("%d\n", i);
+	printf("%d\n", i - 1);
 
 	return (0);
 }
