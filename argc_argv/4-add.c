55ff8d1286cc4948ce55fbd9ae5b45ec215af4a8
ThamerAyachi <thamerjak3@gmail.com>
2022-10-19 12:06:21 +0100

    add 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: 'adds positive numbers.'
+ *
+ * Return:  1 (Success) or 0
+ */
+int main(int argc, char *argv[])
+{
+	int x, i, r = 1;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i] < 'a' || argv[i] > 'z')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		x = strtol(argv[i], NULL, 10);
+		r = r + x;
+	}
+
+	printf("%d\n", r);
+
+	return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -6,3 +6,4 @@
 | 1-args.c        | Write a program that prints the number of arguments passed into it. |
 | 2-args.c        | Write a program that prints all arguments it receives.              |
 | 3-mul.c         | Write a program that multiplies two numbers.                        |
+| 4-add.c         | Write a program that adds positive numbers.                         |
