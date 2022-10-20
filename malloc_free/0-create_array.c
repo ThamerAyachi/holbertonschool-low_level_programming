fdc34d1f919f88ac5d559cf20f752a1fff3b87d5
ThamerAyachi <thamerjak3@gmail.com>
2022-10-20 12:45:43 +0100

    add 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,11 +13,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *p = NULL;
 
 	if (size == 0)
 		return (NULL);
 
-	char *p = malloc(size * sizeof(char));
+	*p = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
