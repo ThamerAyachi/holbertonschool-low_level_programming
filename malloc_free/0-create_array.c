56af6998431de210ee6f8cd5640bf796a790af8f
ThamerAyachi <thamerjak3@gmail.com>
2022-10-20 16:49:35 +0100

    try to fix 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -20,10 +20,13 @@ char *create_array(unsigned int size, char c)
 
 	p = malloc(size * sizeof(char));
 
+	if (p = '\0')
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
-		*(p + i) = c;
+		p[i] = c;
 	}
-	*p = 0;
+
 	return (p);
 }
