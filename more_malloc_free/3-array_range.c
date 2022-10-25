47c044dae37fe1edb8180f0a085beb316d4bbc79
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-25 16:52:06 +0100

    add 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * array_range - Entry point
+ *
+ * @min : integer
+ * @max : integer
+ *
+ * Return: integer[].
+ */
+int *array_range(int min, int max)
+{
+	int *array, i, e;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * (max - min) + 1);
+
+	if (array == 0)
+		return (NULL);
+
+	for (i = min, e = 0; i <= max; i++)
+		array[e++] = i;
+
+	return (array);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -5,3 +5,4 @@
 | 0-malloc_checked.c | Write a function that allocates memory using `malloc`.               |
 | 1-string_nconcat.c | Write a function that concatenates two strings.                      |
 | 2-calloc.c         | Write a function that allocates memory for an array, using `malloc`. |
+| 3-array_range.c    | Write a function that creates an array of integers.                  |
