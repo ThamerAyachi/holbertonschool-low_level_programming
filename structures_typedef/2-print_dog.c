518e589028a3905cf0df5660381ddcfcd9a6d002
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-31 11:29:42 +0100

    fix bug

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-		d->age ? printf("Age: %f\n", d->age) : printf("Age: %f\n", 0);
+		d->age ? printf("Age: %f\n", d->age) : printf("Age: %f\n", 0.000000);
 		d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 }
