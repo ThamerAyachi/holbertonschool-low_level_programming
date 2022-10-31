178f3ecbec5e6229208eef8e6f4f67b0f65b0141
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-31 11:34:59 +0100

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,12 +13,12 @@
  *
  * Description: Dog struct
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
