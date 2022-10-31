204cdc0ab63021543622210716cfffa44fb87431
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-31 12:36:57 +0100

    add 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * free_dog - free
+ *
+ * @d: dog_t
+ *
+ * Description: 'Free dog_t'
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
