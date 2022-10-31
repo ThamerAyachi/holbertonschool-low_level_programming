e86c272d1b4ebccc5d9290bb641c7beb53164670
Thamer Ayachi <thamerjak3@gmail.com>
2022-10-31 12:31:36 +0100

    fix bug

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,39 @@
 #include "dog.h"
 
 /**
+ * strCheck - second function
+ *
+ * @str: string
+ *
+ * Description: 'set string'
+ *
+ * Return: string
+ */
+char *strCheck(char *str)
+{
+	int i = 0, j = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i) != '\0')
+		i++;
+
+	p = malloc(i + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (str[j] != '\0')
+		p[j] = str[j++];
+
+	p[j] = '\0';
+
+	return (p);
+}
+
+/**
  * new_dog - Entry point
  *
  * @name: string
@@ -21,8 +54,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	dog->age = age;
-	dog->name = name;
-	dog->owner = owner;
+
+	dog->name = strCheck(name);
+	if (!dog->name)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = strCheck(owner);
+	if (!dog->owner)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 
 	return (dog);
 }
