c0be75163795aee46f4940d7a5d41c9083cfaa66
Thamer Ayachi <thamerjak3@gmail.com>
2022-11-01 13:57:30 +0100

    final task

---
+++ function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
+ *
+ * Description: calculates basic arithmetic
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	char op;
+	int (*myFunc)(int, int), res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = *(argv[2]);
+
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	myFunc = get_op_func(&op);
+	if (myFunc == NULL || argv[2] == '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	res = myFunc(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", res);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Entry point
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Description: 'add.'
+ *
+ * Return: integer
+ */
+int op_add(int a, int b) { return (a + b); }
+/**
+ * op_sub - Entry point
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Description: 'sub.'
+ *
+ * Return: integer
+ */
+int op_sub(int a, int b) { return (a - b); }
+/**
+ * op_mul - Entry point
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Description: 'mul.'
+ *
+ * Return: integer
+ */
+int op_mul(int a, int b) { return (a * b); }
+/**
+ * op_div - Entry point
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Description: 'div.'
+ *
+ * Return: integer
+ */
+int op_div(int a, int b) { return (a / b); }
+/**
+ * op_mod - Entry point
+ *
+ * @a: integer
+ *
+ * @b: integer
+ *
+ * Description: 'mod.'
+ *
+ * Return: integer
+ */
+int op_mod(int a, int b) { return (a % b); }
