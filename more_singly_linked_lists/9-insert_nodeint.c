b5b46237f8b2be8066468cf490be6f275cb8ce2c
ThamerAyachi <thamerjak3@gmail.com>
2022-11-15 17:52:05 +0100

    fix bug

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,11 +12,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr = malloc(sizeof(listint_t)), *c;
 	unsigned int i;
 
-	if (ptr == NULL)
+	if (ptr == NULL || head == NULL)
 		return (NULL);
 
 	ptr->n = n;
 	c = *head;
+	if (idx == 0)
+	{
+		ptr->next = *head;
+		*head = ptr;
+		return (*head);
+	}
 	for (i = 0; c != NULL; i++)
 	{
 		if (idx == i + 1)
