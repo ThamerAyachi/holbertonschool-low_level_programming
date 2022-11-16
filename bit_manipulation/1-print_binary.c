dba331c20b0400208e4cdf33b3f70cf34526aadc
ThamerAyachi <thamerjak3@gmail.com>
2022-11-16 17:28:11 +0100

    fix bug

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,27 +1,15 @@
 #include "main.h"
 
 /**
- * print_num - print single number
- * @n: unsigned long integer
- */
-void print_num(unsigned long int n)
-{
-	if (n <= 0)
-		return;
-	print_num(n / 2);
-	putchar(n % 2 + '0');
-}
-
-/**
  * print_binary - from base 10 to base 2
  * @n: unsigned long integer
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		putchar('0');
-		return;
-	}
-	print_num(n);
+	unsigned long int v = n;
+
+	if (v > 1)
+		print_binary(v >> 1);
+
+	putchar((v & 1) + '0');
 }
