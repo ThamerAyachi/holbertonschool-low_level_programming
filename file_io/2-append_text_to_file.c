b363b68903e97cee67787f8a324363e9170869ac
ThamerAyachi <thamerjak3@gmail.com>
2022-11-21 16:50:23 +0100

    change 0 by 1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(file);
-		return (0);
+		return (1);
 	}
 	else
 		wr = write(file, text_content, i);
