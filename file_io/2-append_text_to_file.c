8574d24cec9d5d29091e0424826a504f5514c75a
ThamerAyachi <thamerjak3@gmail.com>
2022-11-21 16:54:39 +0100

    try to fix bug

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,27 +8,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, i = 0;
+	int file, i = 0, wr;
 
 	if (filename == NULL)
 		return (-1);
 
 	file = open(filename, O_RDWR | O_APPEND);
+
 	if (file == -1)
 		return (-1);
-
-	while (text_content[i])
-		i++;
 
 	if (text_content == NULL)
 	{
 		close(file);
 		return (1);
 	}
-	else
-	{
-		wr = write(file, text_content, i);
-	}
+
+	while (text_content[i])
+		i++;
+
+	wr = write(file, text_content, i);
 
 	if (wr == -1)
 		return (-1);
