ccc7270c977ad3881a3e26a4d595734c73dce320
ThamerAyachi <thamerjak3@gmail.com>
2022-11-22 12:32:26 +0100

    fix bug

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -50,7 +50,7 @@ int main(int ac, char **av)
 	if (o == -1 || r == -1)
 	{
 		free(str);
-		printf("Error: Can't read from file %s\n", av[1]);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
