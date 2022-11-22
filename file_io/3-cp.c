e162086598e5f2e9e19e90add4538d44ab6bf5e7
ThamerAyachi <thamerjak3@gmail.com>
2022-11-22 12:12:42 +0100

    add 3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - copy file
+ * @ac: integer
+ * @av: list of string
+ * Return: integer
+ */
+int main(int ac, char **av)
+{
+	char *str;
+	int o, r, file, w, len = 0;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	str = malloc(sizeof(char) * 1024 / 8);
+	if (str == NULL)
+		return (-1);
+
+	o = open(av[1], O_RDONLY);
+	r = read(o, str, 1024 / 8);
+
+	if (o == -1 || r == -1)
+	{
+		free(str);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	file = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	while (str[len] != '\0')
+		len++;
+	w = write(file, str, len);
+	if (file == -1 || w == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	if (close(file) == 1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", len);
+		exit(100);
+	}
+
+	return (1);
+}
