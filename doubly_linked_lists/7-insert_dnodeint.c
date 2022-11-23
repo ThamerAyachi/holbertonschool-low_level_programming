29c1b2cf834da881aa303b8deadfe9e23ef1b65f
ThamerAyachi <thamerjak3@gmail.com>
2022-11-23 13:26:21 +0100

    fix bug

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = tmp;
 		return (*h);
 	}
-	while (i < idx - a && c)
+	while (i < idx - 1 && c)
 	{
 		c = c->next;
 		i++;
