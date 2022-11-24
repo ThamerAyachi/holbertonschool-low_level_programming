6995726cdc1a4eaa0e961748cc8469eba0476786
ThamerAyachi <thamerjak3@gmail.com>
2022-11-24 13:51:14 +0100

    fix bug

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index] != NULL)
 	{
-		ht->array[index]->next = tmp;
+		tmp->next = ht->array[index];
+
+		ht->array[index] = tmp;
 		return (1);
 	}
 
