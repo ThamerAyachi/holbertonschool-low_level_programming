362f7c17a2ea747a8024850fecba0fa15a32217c
ThamerAyachi <thamerjak3@gmail.com>
2022-11-24 13:47:53 +0100

    add 3-hash_table_set.c

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - set element
+ * @ht: hash_table_t
+ * @key: constant string
+ * @value: constant string
+ * Return: integer
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (!key || !value)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = malloc(sizeof(hash_node_t));
+
+	if (tmp == NULL)
+		return (0);
+
+	tmp->key = strdup((char *)key);
+	tmp->value = strdup((char *)value);
+	tmp->next = NULL;
+
+	if (ht->array[index] != NULL)
+	{
+		ht->array[index]->next = tmp;
+		return (1);
+	}
+
+	ht->array[index] = tmp;
+	return (1);
+}
